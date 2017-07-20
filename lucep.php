1e75e8f151a2f45139142424e96f8f9bc82158c4
José Roberto García Chico <josergc@gmail.com>
2017-07-20 17:14:56 +0800

    Added account authentication method against gorilla server and better README file and in-plugin texts

--- lucep.php
+++ lucep.php
@@ -1,11 +1,11 @@
 <?php 
 /*
-Plugin Name: lucep 
-Plugin URI: http://lucep.com/api-docs
-Description: Plugin provide lucep script, just enter your account name in Lucep Settings (left side menu under setting menu).
+Plugin Name: Lucep Call Now Button
+Plugin URI: https://lucep.com/signup/?utm_medium=wordpress&utm_campaign=lucep-plugin&utm_source=wordpress-plugin-uri
+Description: Lucep is an instant response call back tool that engages with your website visitors and encourages them to request a call back from your business. Lucep will inform your sales team instantly and provide you with data and analytics on your prospect. 
 Version: 1.0
-Author: lucep
-Author URI: http://lucep.com/api-docs
+Author: Lucep
+Author URI: https://lucep.com/signup/?utm_medium=wordpress&utm_campaign=lucep-plugin&utm_source=wordpress-plugin-author
 License: GPLv3
 Text Domain: lucep
 */
@@ -28,6 +28,105 @@ function register_lucep_plugin_settings_page()
 
 function lucep_plugin_settings_page() 
 {
+    // We check if it is having an action on the page
+    if (!empty($_POST['submit'])) {
+        // User wants to log-in
+        if (
+            !empty($_POST['lucep_accountname']) 
+            && 
+            !empty($_POST['lucep_username']) 
+            && 
+            !empty($_POST['lucep_password']) 
+            ) {
+            // We sanitize the input data
+            $lucep_accountname = sanitize_key($_POST['lucep_accountname']);
+            if ($lucep_accountname != $_POST['lucep_accountname']) {
+                $lucep_accountname = "";
+                ?><div class="error settings-error"><p>Ooops! For team name, only alphanumeric characters are allowed.</p></div><?php
+            }
+            $lucep_username = sanitize_key($_POST['lucep_username']);
+            if ($lucep_username != $_POST['lucep_username']) {
+                $lucep_username = "";
+                ?><div class="error settings-error"><p>Uh-oh!! For user name, only alphanumeric characters are allowed.</p></div><?php
+            }
+        }
+        if (
+            !empty($lucep_accountname) 
+            && 
+            !empty($lucep_username) 
+            && 
+            !empty($_POST['lucep_password']) 
+            ) {
+            $url = "https://gorilla.lucep.com/api/";
+            // The first request is to initialize the handshake
+            $args = [
+                "body" => [
+                    "action" => "login",
+                    "account_key" => $lucep_accountname,
+                    "login_login" => "Login",
+                    "login_username" => $lucep_username,
+                    ],
+                ];
+            $serverFullResponse = wp_remote_post($url,$args);
+            if (!is_wp_error($serverFullResponse)) {
+                if (wp_remote_retrieve_response_code($serverFullResponse) == 200) {
+                    $response = json_decode(wp_remote_retrieve_body($serverFullResponse));
+                    if (!empty($response)) {
+                        if ($response->status == "login") {
+                            // The second request is to create the handshake
+                            $args["body"]["login_challenge"] = $response->challenge;
+                            $args["body"]["login_response"] = hash_hmac(
+                                "sha1",
+                                $response->challenge,
+                                hash_hmac(
+                                    "sha1",
+                                    $response->salt.$lucep_username,
+                                    $_POST['lucep_password'] // This is used for hashing, hence no sanitation is needed
+                                    )
+                                );
+                            $serverFullResponse = wp_remote_post($url,$args);
+                            if (!is_wp_error($serverFullResponse)) {
+                                if (wp_remote_retrieve_response_code($serverFullResponse) == 200) {
+                                    $response = json_decode(wp_remote_retrieve_body($serverFullResponse));
+                                    if (!empty($response)) {
+                                        if ($response->status == "ok") {
+                                            // Because the user can log-in in his/her account, we store the team name in WP's DB
+                                            update_option(
+                                                'lucep_accountname',
+                                                $lucep_accountname
+                                                );
+                                            ?><div class="updated"><p>Updates are saved.</p></div><?php
+                                        } else {
+                                            ?><div class="error settings-error"><p>Your credentials don't seem to be valid. Please check them and try again.</p></div><?php
+                                        }
+                                    } else {
+                                        ?><div class="error settings-error"><p>Whoa! A server somewhere said something that I could not decode... please wait a minute or two and try again!</p></div><?php
+                                    }
+                                } else {
+                                    ?><div class="error settings-error"><p>Argh! The servers seem very busy right now - perhaps try again in a moment.</p></div><?php
+                                }
+                            } else {
+                                ?><div class="error settings-error"><p>Uh-oh! Wordpress said it encountered an error, perhaps try again?</p></div><?php
+                            }
+                        } else {
+                            ?><div class="error settings-error"><p>Hmmm, a server said something unexpected, perhaps try again?</p></div><?php
+                        }
+                    } else {
+                        ?><div class="error settings-error"><p>Hmmm, the server said something unexpected, perhaps try again?</p></div><?php
+                    }
+                } else {
+                    ?><div class="error settings-error"><p>Argh! The server complained there was an error somewhere - this was not your fault though. Perhaps try again?</p></div><?php
+                }
+            } else {
+                ?><div class="error settings-error"><p>Ooops! There was an error in Wordpress, please try again.</p></div><?php
+            }
+        } else if (!empty($_POST["lucep_disconnect"])) {
+            delete_option('lucep_accountname');
+        } else {
+            ?><div class="error settings-error"><p>Settings were not saved.</p></div><?php
+        }
+    }
+    // Print the credentials form if the user didn't log-in yet, otherwise the disconnect form
 ?>
 <link rel="stylesheet" href="<?php echo plugins_url('/css/admin_style.css', __FILE__); ?>">
 <div class="wrap lucep_admin_wrap">
@@ -35,20 +134,43 @@ function lucep_plugin_settings_page()
 		<h2>Lucep Settings</h2>
 	</div>	
 	<div class="main-content">
-		<form method="post" action="options.php">
+		<form method="post" action="">
 			<?php settings_fields( 'lucep_plugin_settings-group' ); ?>
 			<?php do_settings_sections( 'lucep_plugin_settings-group' ); ?>
-			<h3>Script Account Details</h3><hR>
+			<h3>Link up with your Lucep account</h3><hr />
+			<p>Use your Lucep account details below to connect your account with your site. If you do not have a Lucep account just go to <a href="https://lucep.com/signup/?utm_medium=wordpress&utm_campaign=lucep-plugin&utm_source=wordpress-plugin-directory" target="_blank">www.lucep.com/signup</a></p>
+			<?php
+			if (empty(get_option('lucep_accountname'))) { 
+                // This is the credentials form
+			?>
 			<table class="form-table">
 				<tr valign="top">
-					<th scope="row">Account Name</th>
-					<td><input type="text" required  name="lucep_accountname" value="<?php echo esc_attr( get_option('lucep_accountname') ); ?>" /></td>
+					<th scope="row">Team Name</th>
+					<td><input type="text" required  name="lucep_accountname" value="" /></td>
 				</tr>
-				 <tr valign="top">
-					<th scope="row">&nbsp;</th>
-					<td><?php submit_button(); ?></td>
+				<tr valign="top">
+					<th scope="row">User Name</th>
+					<td><input type="text" required  name="lucep_username" value="" /></td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Password</th>
+					<td><input type="password" required  name="lucep_password" value="" /><br /><small><a href="https://lucep.com/set-password" target="_blank">Forgot password?</a></small></td>
+				</tr>
+                <tr valign="top">
+					<td colspan="2"><br/><br/>The Lucep widget will display on your site after your account is linked up.</td>
+				</tr>
+				<tr valign="top">
+					<td colspan="2"><?php submit_button("Link Up"); ?></td>
 				</tr>
 			 </table>
+			 <?php 
+			 } else {
+                // This is the disconnect form
+                ?>Connected using the team '<?php echo esc_attr( get_option('lucep_accountname') ); ?>'. 
+                <input type="hidden" name="lucep_disconnect" value="true" />
+                <?php 
+                submit_button("Disconnect",["red"]); 
+			 } ?>
 		</form>
 	</div>
 </div>
@@ -56,16 +178,19 @@ function lucep_plugin_settings_page()
 
 // add JavaScript code to the footer of every Wordpress page 
 function add_lucep_script_footer()
-{ ?>
+{ 
+$lucep_accountname = esc_attr( get_option('lucep_accountname') );
+?>
 <script type='text/javascript'>
-		window.$gorilla || ( (window._gorilla={
-		no_ui: false,
-		mobile: false,
-		cdn: "https://8d69a4badb4c0e3cd487-efd95a2de0a33cb5b6fcd4ec94d1740c.ssl.cf2.rackcdn.com/",
-		domain: "<?php echo esc_attr( get_option('lucep_accountname') ); ?>",id: 1,lang: "eng"}) 
-		& ( function ( l, u, c, e, p ){ var g = document.createElement(e); g.src = l; g.onload=u; document.getElementsByTagName(c)[p].appendChild(g);})
-		("https://8d69a4badb4c0e3cd487-efd95a2de0a33cb5b6fcd4ec94d1740c.ssl.cf2.rackcdn.com/js/L.SalesGorilla.stable.latest.min.js", null, "head", "script", 0) )
+    window.$gorilla || ( (window._gorilla={
+    no_ui: false,
+    mobile: false,
+    cdn: "https://8d69a4badb4c0e3cd487-efd95a2de0a33cb5b6fcd4ec94d1740c.ssl.cf2.rackcdn.com/",
+    domain: "<?php echo $lucep_accountname; ?>",id: 1,lang: "eng"}) 
+    & ( function ( l, u, c, e, p ){ var g = document.createElement(e); g.src = l; g.onload=u; document.getElementsByTagName(c)[p].appendChild(g);})
+    ("https://8d69a4badb4c0e3cd487-efd95a2de0a33cb5b6fcd4ec94d1740c.ssl.cf2.rackcdn.com/js/L.SalesGorilla.stable.latest.min.js", null, "head", "script", 0) )
 </script>
+<noscript><a href="https://lucep.com/client-enquiry/<?php echo $lucep_accountname; ?>">Click to get a call from our team!</a> <a href="https://lucep.com">Click to call</a> by Lucep</noscript>
 <?php } 
 
 // check user enter account name or not 
@@ -75,7 +200,7 @@ if(!empty($accoutname))
 {
   add_action('wp_footer', 'add_lucep_script_footer');
 }
-else
+else if (empty($_POST['lucep_accountname']))
 {
   add_action( 'admin_notices', 'my_lucep_notice' );
 }
@@ -83,9 +208,9 @@ else
 function my_lucep_notice() 
 {
 ?>
-  <div class="updated">
-      <p><?php _e( 'Please enter your account name in Lucep Settings (left side menu under setting menu),it is required for Lucep plugin to work properly!', 'my_plugin_textdomain' ); ?></p>
+  <div class="error">
+      <p><?php _e( 'Please log-in with your account in Lucep Call Now Button Settings (left side menu under setting menu), it is required for Lucep plugin to work properly!', 'my_plugin_textdomain' ); ?></p>
   </div>
 <?php
 }
-?>
+
---
+++ readme.txt
@@ -0,0 +1,89 @@
+=== Lucep Call Now Button ===
+Contributors: lucep
+Tags: call, contact, customers, sell, sales, leads, convert, conversions, contact plugin, customer support, free call back, click to call, live support, wordpress call now plugin, lucep
+Requires at least: 3.3
+Tested up to: 4.8
+Stable tag: 0.2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+An award winning "call now" (or click to call) widget that works on all of your pages! Proven to increase sales by over 72% and it's free!
+
+== Description ==
+
+Research shows that engaging with prospects in under 5 minutes of requesting a call back, results in 21x higher conversion rates. Lucep's power packed duo of click-to-call widget and mobile app empowers you to do just this, with time and detailed analytics on your side.
+
+[youtube https://www.youtube.com/watch?v=FV08pvInFc4]
+
+###What does the plugin do?
+
+This plugin places a call now widget (click to call) at the bottom of the sceen for all of your visitors. When they click on it, they will be prompted for their name and number, and upon submission, this will be sent to your entire sales team using the Lucep mobile app.
+
+The plugin uses advanced analytics that lets you monitor visitor flow, usage patterns, and understand what your prospect finds most interesting on your site.
+
+###Incredibly easy to start!
+
+1. <a href="https://lucep.com/signup/?utm_medium=wordpress&utm_campaign=lucep-plugin&utm_source=wordpress-plugin-description">Get your free Lucep account</a>
+2. Install this plugin, and activate it
+3. Install the mobile app
+
+###What makes Lucep the number one choice for businesses?
+
+Besides the 42% increase in visitor to lead conversion, Lucep does not have any per minute, or per lead charges. We also offer a pro service for businesses that provides support for integration with CRMs such as SalesForce, Hubspot, Pipedrive and others, along with many other power-packed business features!
+
+== Installation ==
+
+1. Go to 'Plugins' > 'Add new' and type into the search bar: 'Lucep Call Now Button'.
+2. Click 'Install Now'
+3. Click activate
+4. Go to 'Lucep Settings', enter your team name, user name and password
+5. Click 'Link Up' and you're done!
+
+Or:
+
+1. Go to 'Plugins' > 'Add new'
+2. Click 'Upload Plugin'
+3. Click 'Choose File' or 'Browse...' and select the lucep-call-now-button.zip file
+4. Click 'Install Now'
+5. Once installed, click 'Activate Plugin' or activate the plugin through the 'Plugins' menu in WordPress
+6. Go to 'Lucep Settings', enter your team name, user name and password
+7. Click 'Link Up' and you're done!
+
+Once you've got the widget activated in your WordPress installation, you can use the Lucep dashboard to customise all the features of your widget! From colours, to text, to fields!
+
+What are you waiting for? Download the Lucep Call Now Button now and <a href="https://lucep.com/signup/?utm_medium=wordpress&utm_campaign=lucep-plugin&utm_source=wordpress-plugin-install">sign up here</a> for a free account!
+
+== Frequently Asked Questions ==
+
+= Do I have to install any software on my server to make this work? =
+
+Absolutely not. Lucep is a hosted click to call service. The <a href="https://lucep.com/?utm_medium=wordpress&utm_campaign=lucep-plugin&utm_source=wordpress-plugin-faq">Lucep</a> servers will take care of all the heavy lifting for you. Simply configure the plugin, and you're done.
+
+= Is it free to use? =
+
+Lucep has a free plan specially designed for regular Wordpress users. Business users can purchase upgrade options at any time.
+
+= Can I customise the text and colours? =
+
+Yes you can! Once you have installed the widget you can use the Lucep dashboard to customise all the text on the widget, your open and close times, the colours, and pretty much everything else!
+
+= Which web browsers work with this plugin? =
+
+We continually test the compatibility of our widget with all major browsers and full functionality is available for IE11+, and all recent versions of Firefox, Google Chrome, Opera, and Safari. For older browsers, or ones that do not support JavaScript there is a reduced functionality option that gets automatically made available to them.
+
+= Does this plugin communicate with other servers? =
+
+Yes. When you connect your account this plugin needs to check your login details by communicating with the Lucep servers - it then adds the widget code to your WordPress footer. Thereafter, each time a visitor loads a page on your site, the plugin will load the Lucep widget using high speed Akamai CDNs. When a visitor requests for a callback, their name and number is sent securely to the Lucep servers, and then your sales team is instantly notified.
+
+== Screenshots ==
+
+1. Call now button on your website
+2. Once the call now button opens up to prompt a user
+3. The Lucep Call Now Button WordPress setup
+
+
+== Changelog ==
+
+= 0.2.0 =
+* First launch into the WordPress plugin directory!
+
